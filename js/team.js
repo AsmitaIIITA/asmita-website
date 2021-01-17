52c32c7dc64b0d7007dde2014d2643ff6f71ead5
garvitchittora <garvitchittora2016@gmail.com>
2021-01-17 10:56:46 +0530

    team page added with some styling

---
+++ js/team.js
@@ -0,0 +1,82 @@
+Vue.config.devtools = true;
+
+Vue.component('Card', {
+    template: `
+      <div class="card-wrap"
+        @mousemove="handleMouseMove"
+        @mouseenter="handleMouseEnter"
+        @mouseleave="handleMouseLeave"
+        ref="Card">
+        <div class="Card"
+          :style="cardStyle">
+          <div class="img-overflow">
+            <div class="card-Bg" :style="[cardBgTransform, cardBgImage]"></div>
+          </div>
+          <div class="card-info">
+            <slot name="header"><slot name="media"></slot></slot>
+            <slot name="content"></slot>
+          </div>
+        </div>
+      </div>`,
+    mounted() {
+        this.width = this.$refs.Card.offsetWidth;
+        this.height = this.$refs.Card.offsetHeight;
+    },
+    props: ['dataImage'],
+    data: () => ({
+        width: 0,
+        height: 0,
+        mouseX: 0,
+        mouseY: 0,
+        mouseLeaveDelay: null
+    }),
+
+    computed: {
+        mousePX() {
+            return this.mouseX / this.width;
+        },
+        mousePY() {
+            return this.mouseY / this.height;
+        },
+        cardStyle() {
+            const rX = this.mousePX * 20;
+            const rY = this.mousePY * -20;
+            return {
+                transform: `rotateY(${rX}deg) rotateX(${rY}deg)`
+            };
+
+        },
+        cardBgTransform() {
+            const tX = this.mousePX * -30;
+            const tY = this.mousePY * -30;
+            return {
+                transform: `translateX(${tX}px) translateY(${tY}px)`
+            };
+
+        },
+        cardBgImage() {
+            return {
+                backgroundImage: `url(${this.dataImage})`
+            };
+
+        }
+    },
+
+    methods: {
+        handleMouseMove(e) {
+            this.mouseX = e.pageX - this.$refs.Card.offsetLeft - this.width / 2;
+            this.mouseY = e.pageY - this.$refs.Card.offsetTop - this.height / 2;
+        },
+        handleMouseEnter() {
+            clearTimeout(this.mouseLeaveDelay);
+        },
+        handleMouseLeave() {
+            this.mouseLeaveDelay = setTimeout(() => {
+                this.mouseX = 0;
+                this.mouseY = 0;
+            }, 1000);
+        }
+    }
+});
+
+const Team = new Vue({ el: '#app' });

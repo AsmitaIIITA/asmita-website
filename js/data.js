7ef4333c0ae134091deda8e5701f87487dcc34b1
garvitchittora <garvitchittora2016@gmail.com>
2021-01-19 10:53:25 +0530

    team page data

---
+++ js/data.js
@@ -0,0 +1,19 @@
+// $(document).ready(function(){
+//     $.getJSON( "../data/team.json", function( data ) {
+//         var items = [];
+//         console.log("hello");
+//         for (i = 0; i < data["Executives"].length; i++) {
+//             val=data["Executives"][i];
+//             x="";
+//             // x=x+"<Card data-image="+val["image"]+">";
+//             // x=x+"<h1 slot='header'>"+val["name"]+"</h1>";
+//             // x=x+"<p slot='content'>";
+//             // x=x+"<a href="+val["insta"]+" target='__blank'>"+"<i class='fa fa-instagram'></i></a>";
+//             // x=x+"<a href="+val["fb"]+" target='__blank'>"+"<i class='fa fa-facebook'></i></a>";
+//             // x=x+"<br><span>"+val["post"]+"</span></p></Card>";
+//             // items.push(x);
+//             $("#executives").append(x);
+//             console.log(x);
+//         }
+//     });
+// });
